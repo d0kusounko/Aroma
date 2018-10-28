f96df1a233534e05281fe0f075e0418f5fd0a4d8
d0 <do.ventilator@gmail.com>
2018-10-29 04:47:17 +0900

    Min Max関数に不要な指定子がついていたので修正

--- Aroma/include/aroma/common/Algorithm.h
+++ Aroma/include/aroma/common/Algorithm.h
@@ -14,8 +14,8 @@ namespace aroma
 //---------------------------------------------------------------------------
 //!	@brief	指定された値の小さい方を返却.
 //---------------------------------------------------------------------------
-template< class T > static inline
-constexpr const T& Min( const T& a, const T& b )
+template< class T >
+static inline const T& Min( const T& a, const T& b )
 {
 	return ( a < b ) ? a : b;
 }
@@ -23,8 +23,8 @@ constexpr const T& Min( const T& a, const T& b )
 //---------------------------------------------------------------------------
 //!	@brief	指定された値の大きい方を返却.
 //---------------------------------------------------------------------------
-template< class T > static inline
-constexpr const T& Max( const T& a, const T& b )
+template< class T >
+static inline const T& Max( const T& a, const T& b )
 {
 	return ( a > b ) ? a : b;
 }
